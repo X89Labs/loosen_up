dcc9d7a0a89f43e0e0a060e665da57adc5d8a84b
Geoffrey Belanger <geoffrey.belanger@gmail.com>
2025-07-05 10:19:04 -0400

    Initial Commit

---
+++ Loosen Up/Loosen_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loosen_UpApp.swift
+//  Loosen Up
+//
+//  Created by Geoffrey Belanger on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Loosen_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
